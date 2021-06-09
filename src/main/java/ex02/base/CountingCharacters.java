332d86ccdcf0ee04827c8a7dcddb078c74f0fed3
annbinus <72045490+annbinus@users.noreply.github.com>
2021-06-09 12:56:52 -0400

    Update CountingCharacters.java

--- src/main/java/ex02/base/CountingCharacters.java
+++ src/main/java/ex02/base/CountingCharacters.java
@@ -1,7 +1,7 @@
 package ex02.base;
 /*
  *  UCF COP3330 Summer 2021 Assignment 1 Solution
- *  Copyright 2021 Binus Ann
+ *  Copyright 2021 Ann Binus
 
 
 * Exercise 2 - Counting the Number of Characters
